c70eb1ae0c988954701a85dc3a61eb283736803a
Ole Holm Frandsen <froksen@gmail.com>
2012-04-08 21:18:31 +0200

    Previewplayer basic stuff #2

--- source/main.cpp
+++ source/main.cpp
@@ -40,7 +40,6 @@ int main(int argc, char *argv[])
 
 
     MainWindow w;
-
     w.show();
 
 
--- source/previewplayer.cpp
+++ source/previewplayer.cpp
@@ -8,6 +8,13 @@ PreviewPlayer::PreviewPlayer(QWidget *parent) :
     ui->setupUi(this);
     Phonon::VideoPlayer *player =
              new Phonon::VideoPlayer(Phonon::VideoCategory,parent);
+//    player->play(Phonon::MediaSource("/home/froksen/optagelse.avi"));
+
+    ui->gridLayout_3->addWidget(player);
+
+    //Connecting to the Seekslider + volume
+    ui->seekSlider->setMediaObject(player->mediaObject());
+    ui->volumeSlider->setAudioOutput(player->audioOutput());
 }
 
 PreviewPlayer::~PreviewPlayer()
