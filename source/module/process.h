2cf0b9e242f1e12efef7e22ab315e871585605c5
Ole Holm Frandsen <froksen@gmail.com>
2012-05-28 22:04:40 +0200

    Fixed: Added removed files: Process.cpp, process.h, recordingdevices.cpp, recordingdevicess.h

---
+++ source/module/process.h
@@ -0,0 +1,36 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+#include <QtGui>
+#include <QtCore>
+
+
+class process : public QObject
+{
+    Q_OBJECT
+
+private:
+    QString command;
+    QStringList arguments;
+
+
+public:
+    explicit process(QObject *parent = 0);
+    QProcess *mprocess;
+    QByteArray stderrdata;
+    QByteArray stdoutdata;
+
+    void setCommand(QString newCommand);
+    void setArguments(QStringList newArguments);
+    void startCommand();
+    void stopCommand();
+
+private slots:
+    void readstdout();
+    void readstderr();
+    void onProcessFinished(int exitCode, QProcess::ExitStatus status);
+
+
+
+};
+
+#endif // PROCESS_H
