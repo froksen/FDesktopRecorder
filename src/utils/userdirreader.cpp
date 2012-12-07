28db833ffad1f0403c0952a7b6a298b4a25534a3
Ole Holm Frandsen <froksen@gmail.com>
2012-12-07 17:06:54 +0100

    Updated Changelog

--- src/utils/userdirreader.cpp
+++ src/utils/userdirreader.cpp
@@ -22,14 +22,18 @@ bool UserdirReader::readFile(QString filename)
 {
     QFile mFile(filename);
     if(!mFile.open(QIODevice::ReadOnly | QIODevice::Text)){
+        qDebug() << "XDG (user-dirs.dirs) not found";
         return false;
     }
+
+    qDebug() << "XDG (user-dirs.dirs) found:" << mFile.fileName();
 
     QTextStream in(&mFile);
     QString line = in.readLine();
     while (!line.isNull()) {
         if(line.startsWith("XDG_VIDEOS_DIR")){
             xdg_videos_dir = line.remove("XDG_VIDEOS_DIR=").remove(0,1).remove(-1,1).replace("$HOME",QDir::homePath());
+            qDebug() << "XDG_VIDEOS_DIR:" << xdg_videos_dir;
         }
         line = in.readLine();
     }
