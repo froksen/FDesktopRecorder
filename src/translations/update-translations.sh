c34d98dfb806c934a6e003def90363eb3f448c8c
Ole Holm Frandsen <froksen@gmail.com>
2013-05-16 11:31:54 +0200

    removed script

--- src/translations/update-translations.sh
+++
@@ -1,9 +0,0 @@
-#Update *.ts files
-
-echo "------------------|Updating *.ts files|------------------"
-lupdate ../*.cpp ../*.h ../ui/*.ui -ts  *.ts
-
-echo ""
-#Create the releases
-echo "------------------|Updating *.qm files|------------------"
-lrelease FDesktopRecorder_*.ts
